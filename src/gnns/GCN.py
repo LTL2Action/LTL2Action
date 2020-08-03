bbea3e827041ad1f9e3bc662118c8c7f7f165969
Andrew Chong Li <acli@edu.uwaterloo.ca>
2020-08-03 16:50:29 -0400

    Share convolutional layer params

--- src/gnns/GCN.py
+++ src/gnns/GCN.py
@@ -7,21 +7,23 @@ import dgl
 from dgl.nn.pytorch.conv import GraphConv
 
 class GCN(nn.Module):
-    def __init__(self, input_dim, output_dim, hidden_dims = [32]):
+    def __init__(self, input_dim, output_dim, hidden_dim = 32, num_layers = 2):
         super(GCN, self).__init__()
-        self.num_layers = len(hidden_dims)
-        self.convs = nn.ModuleList([GraphConv(in_dim, out_dim) for (in_dim, out_dim)
-                      in zip([input_dim] + hidden_dims[:-1], hidden_dims)])
-
-        self.g_embed = nn.Linear(hidden_dims[-1], output_dim)
+        self.num_layers = num_layers
+        self.linear_in = nn.Linear(input_dim, hidden_dim)
+        self.conv = GraphConv(hidden_dim, hidden_dim)
+        self.g_embed = nn.Linear(hidden_dim, output_dim)
 
     # Uses the base implementation which averages hidden representations of all nodes
     def forward(self, g):
         g = np.array(g).reshape((1, -1)).tolist()[0]
         g = dgl.batch(g)
         h = g.ndata["feat"].float()
+        h = self.linear_in(h)
+
+        # Apply convolution layers
         for i in range(self.num_layers):
-            h = F.relu(self.convs[i](g, h))
+            h = F.relu(self.conv(g, h))
         g.ndata['h'] = h
 
         # Calculate graph representation by averaging all the hidden node representations.
@@ -31,15 +33,18 @@ class GCN(nn.Module):
 
 # GCN, but the graph representation is only the representation of the root node.
 class GCNRoot(GCN):
-    def __init__(self, input_dim, output_dim, hidden_dims = [32]):
-        super(GCNRoot, self).__init__(input_dim, output_dim, hidden_dims)
+    def __init__(self, input_dim, output_dim, hidden_dim = 32, num_layers = 2):
+        super(GCNRoot, self).__init__(input_dim, output_dim, hidden_dim, num_layers)
 
     def forward(self, g):
         g = np.array(g).reshape((1, -1)).tolist()[0]
         g = dgl.batch(g)
         h = g.ndata["feat"].float()
+        h = self.linear_in(h)
+
+        # Apply convolution layers
         for i in range(self.num_layers):
-            h = F.relu(self.convs[i](g, h))
+            h = F.relu(self.conv(g, h))
         g.ndata['h'] = h
 
         gs = dgl.unbatch(g)
