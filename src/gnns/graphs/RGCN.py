9b5a263abd43a64294a3937831cb70782bc7bdf1
Andrew Chong Li <acli@edu.uwaterloo.ca>
2020-08-14 18:42:46 -0400

    Add code for appending h_0 to each convolution

--- src/gnns/graphs/RGCN.py
+++ src/gnns/graphs/RGCN.py
@@ -11,23 +11,34 @@ from gnns.graphs.GNN import GNN
 from utils.ast_builder import edge_types
 
 class RGCN(GNN):
-    def __init__(self, input_dim, output_dim, **kwargs):
-        super().__init__(input_dim, output_dim)
+    def __init__(self, input_dim, output_dim, append_h0, **kwargs):
+        super().__init__(input_dim, output_dim, append_h0)
 
+        self.append_h0 = append_h0
         hidden_dims = kwargs.get('hidden_dims', [32])
         self.num_layers = len(hidden_dims)
-        self.convs = nn.ModuleList([RelGraphConv(in_dim, out_dim, len(edge_types), activation=F.relu)
-            for (in_dim, out_dim) in zip([input_dim] + hidden_dims[:-1], hidden_dims)])
+
+        if self.append_h0:
+            hidden_plus_input_dims = [hd + input_dim for hd in hidden_dims]
+            self.convs = nn.ModuleList([RelGraphConv(in_dim, out_dim, len(edge_types), activation=F.relu)
+                for (in_dim, out_dim) in zip([input_dim] + hidden_plus_input_dims[:-1], hidden_dims)])
+        else:
+            self.convs = nn.ModuleList([RelGraphConv(in_dim, out_dim, len(edge_types), activation=F.relu)
+                for (in_dim, out_dim) in zip([input_dim] + hidden_dims[:-1], hidden_dims)])
 
         self.g_embed = nn.Linear(hidden_dims[-1], output_dim)
 
     def forward(self, g):
         g = np.array(g).reshape((1, -1)).tolist()[0]
         g = dgl.batch(g)
-        h = g.ndata["feat"].float()
+        h_0 = g.ndata["feat"].float()
+        h = h_0
         etypes = g.edata["type"].float()
         for i in range(self.num_layers):
-            h = self.convs[i](g, h, etypes)
+            if self.append_h0 and i != 0:
+                h = self.convs[i](g, torch.cat([h, h_0], dim=1), etypes)
+            else:
+                h = self.convs[i](g, h, etypes)
         g.ndata['h'] = h
 
         # Calculate graph representation by averaging all the hidden node representations.
@@ -36,18 +47,21 @@ class RGCN(GNN):
 
 
 class RGCNRoot(RGCN):
-    def __init__(self, input_dim, output_dim, **kwargs):
-        super().__init__(input_dim, output_dim, **kwargs)
+    def __init__(self, input_dim, output_dim, append_h0, **kwargs):
+        super().__init__(input_dim, output_dim, append_h0, **kwargs)
 
     def forward(self, g):
         g = np.array(g).reshape((1, -1)).tolist()[0]
         g = dgl.batch(g)
 
-        h = g.ndata["feat"].float().squeeze()
+        h_0 = g.ndata["feat"].float().squeeze()
+        h = h_0
         etypes = g.edata["type"]
         for i in range(self.num_layers):
-            h = self.convs[i](g, h, etypes)
+            if self.append_h0 and i != 0:
+                h = self.convs[i](g, torch.cat([h, h_0], dim=1), etypes)
+            else:
+                h = self.convs[i](g, h, etypes)
         g.ndata['h'] = h # TODO (Pashootan): Check if this is redundant
-
         hg = dgl.sum_nodes(g, 'h', weight='is_root')
         return self.g_embed(hg).squeeze(1)
