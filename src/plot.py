6cdb00e3dc3aa33445f5a187b7416242297807a2
Andrew Chong Li <acli@edu.uwaterloo.ca>
2021-01-02 01:47:18 -0500

    Update plotting tools

--- src/plot.py
+++ src/plot.py
@@ -4,105 +4,230 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as sp
 
+MAX_HORIZON = 1000000000    
 
-def read_data(tf_file, tag="return_mean"):
-	data = []
-	frames = []
-	done = False
-	for e in tf.train.summary_iterator(tf_file):
-		for v in e.summary.value:
-			if v.tag == "frames":
-				frames.append(v.simple_value)
-				if v.simple_value > MAX_HORIZON:
-					done = True
-			elif v.tag == tag:
-				data.append(v.simple_value)
-		if done:
-			break
+def read_data(tf_dir, tag="return_mean"):
 
-	data = np.array(data[:min(len(frames), len(data))])
-	frames = np.array(frames[:min(len(frames), len(data))])
+    list_of_data = []
+
+    tf_files = glob.glob(tf_dir + "events.out.tfevents*")
+
+    for tf_file in tf_files:
+        done = False
+        data = []
+        frames = []
+
+        for e in tf.train.summary_iterator(tf_file):
+            for v in e.summary.value:
+                if v.tag == "frames":
+                    frames.append(v.simple_value)
+                    if v.simple_value > MAX_HORIZON:
+                        done = True
+                elif v.tag == tag:
+                    data.append(v.simple_value)
+            if done:
+                break
+
+        data = data[:min(len(frames), len(data))]
+        frames = frames[:min(len(frames), len(data))]
+
+        if len(data) != 0:
+            list_of_data.append((data, frames))
+
+    list_of_data = sorted(list_of_data, key=(lambda pair: min(pair[1])))
+
+    cleaned_frames = []
+    cleaned_data = []
+    max_frames = 0
+    for (data, frames) in list_of_data:
+        for i in range(len(data)):
+            if frames[i] < max_frames:
+                continue
+            else:
+                max_frames = frames[i]
+                cleaned_frames.append(frames[i])
+                cleaned_data.append(data[i])
 
 
-	return frames, data
+    # Cumulative sum smoothing
+    window_width = 10
+    cumsum_vec = np.cumsum(np.insert(cleaned_data, 0, 0)) 
+    ma_vec = (cumsum_vec[window_width:] - cumsum_vec[:-window_width]) / window_width
+    return cleaned_frames[:len(ma_vec)], ma_vec
 
 # Returns (X_data, (Y_average, Y_error))
-def read_data_and_average(tf_files, tag="return_mean"):
-	X_all = None
-	Ys = []
+def read_data_and_average(tf_dirs, tag="return_mean"):
+    X_all = None
+    Ys = []
 
-	for file in tf_files:
-		X, Y = read_data(file, tag)
+    for tf_dir in tf_dirs:
+        X, Y = read_data(tf_dir, tag)
 
-		Ys.append(Y)
-		if X_all is None:
-			X_all = X
-		else:
-			if not np.array_equal(X_all, X):
-				print("ERROR: X axis values must all be the same")
-				print(X_all[-100:])
-				print(X[-100:])
-				exit(1)
+        Ys.append(Y)
+        if X_all is None:
+            X_all = X
+        else:
+            if len(X) < len(X_all):
+                X_all = X
+        # print(tf_dir, len(Y))
 
-	N = len(Ys[0])
-	M = len(Ys)
-	avgs = [0 for i in range(N)]
-	lower = [0 for i in range(N)]
-	upper = [0 for i in range(N)]
+    N = min([len(Ys[i]) for i in range(len(Ys))])
+    M = len(Ys)
+    print(M, N, [len(Ys[i]) for i in range(len(Ys))])
+    
 
-	for n in range(N):
-		vals = []
-		for m in range(M):
-			vals.append(Ys[m][n])
+    avgs = [0 for i in range(N)]
+    lower = [0 for i in range(N)]
+    upper = [0 for i in range(N)]
 
-		vals = np.array(vals)
-		if M == 1:
-			lo = hi = np.mean(vals)
-		else:
-			lo, hi = sp.t.interval(0.9, len(vals)-1, loc=np.mean(vals), scale=sp.sem(vals))
+    for n in range(N):
+        vals = []
+        for m in range(M):
+            vals.append(Ys[m][n])
 
-		avgs[n] = (lo+hi)/2
-		lower[n] = lo
-		upper[n] = hi
+        vals = np.array(vals)
+        if M == 1:
+            lo = hi = np.mean(vals)
+        else:
+            lo, hi = sp.t.interval(0.9, len(vals)-1, loc=np.mean(vals), scale=sp.sem(vals))
 
-	return X_all, avgs, lower, upper
+        avgs[n] = (lo+hi)/2
+        lower[n] = lo
+        upper[n] = hi
+
+    return X_all, avgs, lower, upper
 
 
-# Logs for Until_3_3_1_1 
-# A_logs = glob.glob("storage/3_3_1_1/RGCN*_UntilTasks_3_3_1_1_Letter-7x7*/train/events.out.tfevents*")
-# B_logs = glob.glob("storage/3_3_1_1/rnn-pretrained_UntilTasks_3_3_1_1_Letter-7x7-v3_seed:*_epochs:4_bs:256_fpp:None_dsc:0.99_lr:0.001_ent:0.01_clip:0.2/train/events.out.tfevents*")
-# D_logs = glob.glob("storage/3_3_1_1/rnn_UntilTasks_3_3_1_1_Letter-7x7-v3_seed:*_epochs:4_bs:256_fpp:None_dsc:0.99_lr:0.001_ent:0.01_clip:0.2/train/events.out.tfevents*")
-# lines = [(A_logs, "GNN Pretrained"), (B_logs, "RNN Pretrained"), (D_logs, "RNN From scratch")]
-# MAX_HORIZON = 2900000
+# # Letter-Env UNTIL_1_3_1_2
+A_logs = glob.glob("storage-good/RGCN/*Until*/train/")
+B_logs = glob.glob("storage-good/GRU/*Until*/train/")
+C_logs = glob.glob("storage-good/LSTM/*Until*/train/")
+D_logs = glob.glob("storage-good/Myopic/*Until*/train/")
+E_logs = glob.glob("storage-good/GRU-no-progression/*Until*/train/")
+F_logs = glob.glob("storage-good/No-LTL/*Until*/train/")
+lines = [(A_logs, "GNN+progression"), (B_logs, "GRU+progression"), (C_logs, "LSTM+progression"), (D_logs, "Myopic"), (E_logs, "GRU"), (F_logs, "No LTL")]
+MAX_HORIZON = 20000000
+title = "Letter-Env: Until_1_3_1_2"
 
-# Logs for Until_1_3_1_2
-A_logs = glob.glob("storage/1_3_1_2/rnn-pretrained_UntilTasks_1_3_1_2*lr:0.001*/train/events.out.tfevents*")
-B_logs = glob.glob("storage/1_3_1_2/rnn_UntilTasks_1_3_1_2*lr:0.001*/train/events.out.tfevents*")
-C_logs =  glob.glob("storage/1_3_1_2/rnn-pretrained-unfreeze_ltl_UntilTasks_1_3_1_2*epochs:8*lr:0.001*/train/events.out.tfevents*")
-D_logs = glob.glob("storage/1_3_1_2/RGCN_4x16*/train/events.out.tfevents*")
 
-lines = [(B_logs, "From scratch"), (C_logs, "Pretrained (not frozen)"), (D_logs, "RGCN (not frozen)")]
-MAX_HORIZON = 19000000
-plt.title("Until_1_3_1_2")
+# # Letter-Env EVENTUALLY_1_5_1_4
+# A_logs = glob.glob("storage-good/RGCN/*Eventually*/train/")
+# B_logs = glob.glob("storage-good/GRU/*Eventually*/train/")
+# C_logs = glob.glob("storage-good/LSTM/*Eventually*/train/")
+# D_logs = glob.glob("storage-good/Myopic/*Eventually*/train/")
+# E_logs = glob.glob("storage-good/GRU-no-progression/*Eventually*/train/")
+# F_logs = glob.glob("storage-good/No-LTL/*Eventually*/train/")
 
-# Logs for Sequence_2_4
-# A_logs = glob.glob("storage/Sequence_2_4/rnn-pretrained_Sequence_2_4*lr:0.001*/train/events.out.tfevents*")
-# B_logs = glob.glob("storage/Sequence_2_4/rnn_Sequence_2_4*lr:0.001*/train/events.out.tfevents*")
-# lines = [(A_logs, "Pretrained"), (B_logs, "From scratch")]
-# MAX_HORIZON = 700000
+# lines = [(A_logs, "GNN+progression"), (B_logs, "GRU+progression"), (C_logs, "LSTM+progression"), (D_logs, "Myopic"), (E_logs, "GRU"), (F_logs, "No LTL")]
+# MAX_HORIZON = 20000000
+# title = "Letter-Env: Eventually_1_5_1_4"
 
-# Logs for pretraining 1_3_1_2 GNN vs RNN 
-# A_logs = glob.glob("storage/pretraining_1_3_1_2/RGCN*/train/events.out.tfevents*")
-# B_logs = glob.glob("storage/pretraining_1_3_1_2/rnn*/train/events.out.tfevents*")
-# lines = [(A_logs, "GNN"), (B_logs, "RNN")]
-# MAX_HORIZON = 700000
+# #Symbol-Env UNTIL_1_3_1_2
+
+# C_logs = glob.glob("symbol-storage/RGCN/*Until_1_3_1_2*/train/")
+# D_logs = glob.glob("symbol-storage/GRU/*Until_1_3_1_2*/train/")
+
+# lines = [(C_logs, "GNN"), (D_logs, "GRU")]
+# title = "Symbol Env: Until_1_3_1_2"
+
+
+# #Symbol-Env EVENTUALLY_1_5_1_4
+# A_logs = glob.glob("symbol-storage/RGCN/*Eventually_1_5_1_4*/train/")
+# B_logs = glob.glob("symbol-storage/GRU/*Eventually_1_5_1_4*epochs:4*lr:0.003*/train/")
+
+# lines = [(A_logs, "GNN"), (B_logs, "GRU")]
+# title = "Symbol Env: Eventually_1_5_1_4"
+
+#=============================================================================================================================================
+# ## NOTE: For the transfer results you need to comment out the code at the bottom. 
+
+# # Transfer Until_1_3_1_2
+# A1_logs = glob.glob("storage-good/RGCN/*Until_1_3_1_2*/train/")
+# A2_logs = glob.glob("transfer-storage/RGCN/*pretrained_Until_1_3_1_2*/train/")
+# A3_logs = glob.glob("transfer-storage/RGCN/*pretrained-freeze_ltl_Until_1_3_1_2*bs:64*/train/")
+
+# B1_logs = glob.glob("storage-good/GRU/*Until_1_3_1_2*/train/")
+# B2_logs = glob.glob("transfer-storage/GRU/*pretrained_Until_1_3_1_2*/train/")
+# B3_logs = glob.glob("transfer-storage/GRU/*pretrained-freeze_ltl_Until_1_3_1_2*/train/")
+
+# lines_A = [(A1_logs, "GNN", "-"), (A2_logs, "GNN+pretraining", "dotted")]
+# lines_B = [(B1_logs, "GRU", "-"), (B2_logs, "GRU+pretraining", "dotted")]
+# MAX_HORIZON = 10000000
+
+# title = "Transfer - Letter-Env: Until_1_3_1_2"
+# plt.ylabel("Discounted return")
+# plt.xlabel("Frames")
+
+# for data_line, label, linestyle in lines_A:
+#     X, Y_avg, Y_lower, Y_upper = read_data_and_average(data_line, tag="average_discounted_return") #return_mean, average_discounted_return, average_reward_per_step
+#     plt.plot(X, Y_avg, linewidth = 1, label=label, linestyle=linestyle, color="b")
+#     plt.fill_between(X, Y_lower, Y_upper, alpha=0.2, color="b")
+
+# for data_line, label, linestyle in lines_B:
+#     X, Y_avg, Y_lower, Y_upper = read_data_and_average(data_line, tag="average_discounted_return") #return_mean, average_discounted_return, average_reward_per_step
+#     plt.plot(X, Y_avg, linewidth = 1, label=label, linestyle=linestyle, color="orange")
+#     plt.fill_between(X, Y_lower, Y_upper, alpha=0.2, color="orange")
+
+# plt.legend()
+# plt.show()
+
+#=============================================================================================================================================
+
+
+#  #Transfer EVENTUALLY_1_5_1_4
+# A1_logs = glob.glob("storage-good/RGCN/*Eventually_1_5_1_4*/train/")
+# A2_logs = glob.glob("transfer-storage/RGCN/*pretrained_Eventually_1_5_1_4*/train/")
+# A3_logs = glob.glob("transfer-storage/RGCN/*pretrained-freeze_ltl_Eventually_1_5_1_4*bs:64*/train/")
+
+# B1_logs = glob.glob("storage-good/GRU/*Eventually_1_5_1_4*/train/")
+# B2_logs = glob.glob("transfer-storage/GRU/*pretrained_Eventually_1_5_1_4*/train/")
+# B3_logs = glob.glob("transfer-storage/GRU/*pretrained-freeze_ltl_Eventually_1_5_1_4*/train/")
+
+# lines_A = [(A1_logs, "GNN", "-"), (A2_logs, "GNN+pretraining", "dotted")]
+# lines_B = [(B1_logs, "GRU", "-"), (B2_logs, "GRU+pretraining", "dotted")]
+# MAX_HORIZON = 10000000
+# title = "Transfer - Letter-Env: Eventually_1_5_1_4"
+# plt.ylabel("Discounted return")
+# plt.xlabel("Frames")
+
+# for data_line, label, linestyle in lines_A:
+#     X, Y_avg, Y_lower, Y_upper = read_data_and_average(data_line, tag="average_discounted_return") #return_mean, average_discounted_return, average_reward_per_step
+#     plt.plot(X, Y_avg, linewidth = 1, label=label, linestyle=linestyle, color="b")
+#     plt.fill_between(X, Y_lower, Y_upper, alpha=0.2, color="b")
+
+# for data_line, label, linestyle in lines_B:
+#     X, Y_avg, Y_lower, Y_upper = read_data_and_average(data_line, tag="average_discounted_return") #return_mean, average_discounted_return, average_reward_per_step
+#     plt.plot(X, Y_avg, linewidth = 1, label=label, linestyle=linestyle, color="orange")
+#     plt.fill_between(X, Y_lower, Y_upper, alpha=0.2, color="orange")
+
+# plt.legend()
+# plt.show()
+
+#=============================================================================================================================================
+
+
+# # Safety
+# A_logs = glob.glob("tmp-storage/GRU-pretrained*bs:2048*fpp:2048*lr:0.0003*ent:0.003*prog:full/train/")
+# B_logs = glob.glob("tmp-storage/GRU_*bs:1024*fpp:4096*lr:0.0001*ent:0.001*prog:partial/train/")
+
+# lines = [(A_logs, "Best GNN so far"),(B_logs, "Best Myopic")]
+
+
+#=========================================
+plt.figure(figsize=(12,6))
+plt.ylabel("Discounted return")
+plt.xlabel("Frames")
+plt.title(title)
 
 for data_line, label in lines:
-	X, Y_avg, Y_lower, Y_upper = read_data_and_average(data_line)
-	plt.plot(X, Y_avg, linewidth = 1, label=label)
-	plt.fill_between(X, Y_lower, Y_upper, alpha=0.2)
+    X, Y_avg, Y_lower, Y_upper = read_data_and_average(data_line, tag="average_discounted_return") #return_mean, average_discounted_return, average_reward_per_step
+    plt.plot(X, Y_avg, linewidth = 1, label=label)
+    plt.fill_between(X, Y_lower, Y_upper, alpha=0.2)
+
 
 
 
 plt.legend()
+# plt.savefig("figs/" + title + ".pdf")
 plt.show()
+
